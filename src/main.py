1a7a1a74b51221d4eed3dd33ec6d783f7bffa88c
Oleg Barmin <obarmin@cisco.com>
2020-11-05 21:12:23 +0200

    Remove redundant to-do

--- src/main.py
+++ src/main.py
@@ -35,6 +35,5 @@ if __name__ == '__main__':
     print("------- General Stats -------")
     print(table)
 
-    # todo: jobs in the system
     # todo: probability of system being idle
     # todo: probability of reject
